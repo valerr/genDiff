1f7708f37a2cbd52ddc037a1469b0225f9b27770
valerr <spokich394@gmail.com>
2019-11-26 11:59:10 +0300

    step 2 add arguments, format option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,9 @@ import program from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .action()
 
 program.parse(process.argv);
-
-// if (!program.args.length) program.help();
